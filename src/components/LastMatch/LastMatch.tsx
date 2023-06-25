94cdda30f8efea77c7dd43018865ced412c4cbf6
Hakim Barraud <hakim.barraud@gmail.com>
2023-06-25 11:45:26 +0200

    Finish the LastMatch component

--- src/components/LastMatch/LastMatch.tsx
+++ src/components/LastMatch/LastMatch.tsx
@@ -1,10 +1,21 @@
-import React from "react";
 import useLastMatch from "./useLastMatch";
+import "./LastMatch.scss";
 
 const LastMatch = () => {
   const { data } = useLastMatch();
-  console.log(data);
-  return <div>LastMatch</div>;
+  return (
+    <div className="stats-container">
+      <h2 className="heading">Dernier match</h2>
+      <div className="team-score">
+        <p className="name">{data?.left_team.name}</p>
+        <p className="score red-score">{data?.left_team.score}</p>
+      </div>
+      <div className="team-score">
+        <p className="name">{data?.right_team.name}</p>
+        <p className="score green-score">{data?.right_team.score}</p>
+      </div>
+    </div>
+  );
 };
 
 export default LastMatch;
